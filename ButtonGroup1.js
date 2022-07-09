781e1d427e4435466fa3bbf71f46dabf497bdc73
xiansheng123 <732824600@qq.com>
2022-07-09 12:04:20 +0800

    add react-router-dom

--- ButtonGroup1.js
+++ ButtonGroup1.js
@@ -21,6 +21,4 @@ export class ButtonGroup1 extends Component {
             </div>
         );
     }
-
-
 }
---
+++ Invoices.js
@@ -0,0 +1,7 @@
+export default function Invoices() {
+    return (
+        <main style={{padding: "1rem"}}>
+            <h2>Invoices</h2>
+        </main>
+    );
+}
